544e15dfd561e5bd5a239d0f149289eccddb4ed5
artsci-studio <137820967+artsci-studio@users.noreply.github.com>
2023-07-03 15:01:33 -0500

    Update find.js

--- find.js
+++ find.js
@@ -256,7 +256,7 @@ function toggleDropdown(event) {
   // Toggle clicked dropdown
   clickedDropdown.classList.toggle("open");
   clickedDropdown.setAttribute('aria-expanded', "true");
-  console.log("opened");
+  console.log("open dropdown");
 }
 
 function closeDropdown(event) {
