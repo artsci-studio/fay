deeb5b83de3000a44a1b407b9dbdff3eb64c8861
artsci-studio <137820967+artsci-studio@users.noreply.github.com>
2024-04-23 11:52:19 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,14 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const hoverTabElements = document.querySelectorAll('[ms-code-onhover="click"]');
 
-  hoverTabElements.forEach(hoverTabElement => {
-    hoverTabElement.addEventListener('mouseenter', function() {
-      hoverTabElement.click();
-    });
+const tabs = document.querySelectorAll('.nav_tab-link');
+const tabPanes = document.querySelectorAll('.nav_tab-pane');
+
+tabs.forEach((tab, tabIndex) => {
+  tab.addEventListener('mouseover', () => {
+      // Remove the 'active' class from all tabs and tab panes
+      tabs.forEach(t => t.classList.remove('active'));
+      tabPanes.forEach(pane => pane.classList.remove('active'));
+
+      // Add the 'active' class to the hovered tab and corresponding tab pane
+      tab.classList.add('active');
+      tabPanes[tabIndex].classList.add('active');
   });
 });
-
-
 
 let menuWrap = $(".mobile-menu");
 let menuPanels = $(".menu-panel");
@@ -22,44 +26,44 @@ let previouslyFocused;
 let active;
 
 let showSubMenu = gsap.timeline({
-  paused: true,
-  onComplete: () => {
-    active.find("a, button").first().focus();
-  },
-  onReverseComplete: () => {
-    previouslyFocused.focus();
-  }
+paused: true,
+onComplete: () => {
+  active.find("a, button").first().focus();
+},
+onReverseComplete: () => {
+  previouslyFocused.focus();
+}
 });
 showSubMenu.to(menuPanels, {
-  x: "-100%",
-  ease: "power2.inOut",
-  duration: 0.4
+x: "-100%",
+ease: "power2.inOut",
+duration: 0.4
 });
 
 let showSubSubMenu = gsap.timeline({
-  paused: true,
-  onComplete: () => {
-    active.find("a, button").first().focus();
-  },
-  onReverseComplete: () => {
-    backFirst.focus();
-  }
+paused: true,
+onComplete: () => {
+  active.find("a, button").first().focus();
+},
+onReverseComplete: () => {
+  backFirst.focus();
+}
 });
 showSubSubMenu.to(
-  menuPanels, { x: "-200%" });
+menuPanels, { x: "-200%" });
 
 let showMainMenu = gsap.timeline({
-  paused: true,
-  defaults: { duration: 0.3 },
-  onReverseComplete: () => {
-    showSubSubMenu.progress(0);
-    showSubSubMenu.pause();
-    showSubMenu.progress(0);
-    showSubMenu.pause();
-  },
-  onComplete: () => {
-    menuWrap.find("a").first().focus();
-  }
+paused: true,
+defaults: { duration: 0.3 },
+onReverseComplete: () => {
+  showSubSubMenu.progress(0);
+  showSubSubMenu.pause();
+  showSubMenu.progress(0);
+  showSubMenu.pause();
+},
+onComplete: () => {
+  menuWrap.find("a").first().focus();
+}
 
 });
 
@@ -71,53 +75,53 @@ showMainMenu.to(menuLines.eq(1), { width: "0px" }, "<");
 showMainMenu.to(menuLines.eq(2), { y: -8, rotate: -45 }, "<");
 
 menuButton.on("click", function () {
-  if (showMainMenu.progress() === 0) {
-    showMainMenu.play();
-  } else {
-    showMainMenu.reverse();
-  }
+if (showMainMenu.progress() === 0) {
+  showMainMenu.play();
+} else {
+  showMainMenu.reverse();
+}
 });
 
 $(".opens-more").on("click", function () {
-  previouslyFocused = $(this);
-  let linkIndex = $(this).index();
-  linkIndex = linkIndex - 2;
-  console.log(linkIndex);
-  showSubMenu.play();
-  contentMore.hide();
-  active = contentMore.eq(linkIndex).show();
+previouslyFocused = $(this);
+let linkIndex = $(this).index();
+linkIndex = linkIndex - 2;
+console.log(linkIndex);
+showSubMenu.play();
+contentMore.hide();
+active = contentMore.eq(linkIndex).show();
 });
 
 backFirst.on("click", function () {
-  showSubMenu.reverse();
+showSubMenu.reverse();
 });
 
 $(".opens-sub").on("click", function () {
-  let linkIndex = $(this).index();
-  showSubSubMenu.play();
-  contentSub.hide();
-  active = contentSub.eq(linkIndex).show();
+let linkIndex = $(this).index();
+showSubSubMenu.play();
+contentSub.hide();
+active = contentSub.eq(linkIndex).show();
 });
 
 backSecond.on("click", function () {
-  showSubSubMenu.reverse();
+showSubSubMenu.reverse();
 });
 
 $("[trap-focus]").each(function () {
-  let focusBack = $(this).attr("focus-back");
-  let lastItem = $(this).find("a, button").last();
-  lastItem.on("keydown", function (e) {
-    if (e.key === "Tab") {
-      e.preventDefault();
-      if (focusBack === "first") {
-        backFirst.focus();
-      }
-      else if (focusBack === "second") {
-        backSecond.focus();
-      } else {
-        menuButton.focus();
-      }
-
+let focusBack = $(this).attr("focus-back");
+let lastItem = $(this).find("a, button").last();
+lastItem.on("keydown", function (e) {
+  if (e.key === "Tab") {
+    e.preventDefault();
+    if (focusBack === "first") {
+      backFirst.focus();
     }
-  })
+    else if (focusBack === "second") {
+      backSecond.focus();
+    } else {
+      menuButton.focus();
+    }
+
+  }
+})
 })
